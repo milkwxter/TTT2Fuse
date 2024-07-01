6a5ce00cf0cfb36212391429503c9da2334438ad
zaqlul9595 <cheezbawlz123@gmail.com>
2024-07-01 14:10:01 -0500

    Added convar for fuse timer and kill refresh. Integrated with F1 menu of course.

--- ttt2-role_fuse/lua/terrortown/lang/en/fuse.lua
+++ ttt2-role_fuse/lua/terrortown/lang/en/fuse.lua
@@ -7,3 +7,6 @@ L["body_found_" .. FUSE.abbr] = "They were a Fuse."
 L["search_role_" .. FUSE.abbr] = "This person was a Fuse!"
 L["target_" .. FUSE.name] = "Fuse"
 L["ttt2_desc_" .. FUSE.name] = [[The Fuse must kill other players quickly, or else he explodes!]]
+
+--Convar Label
+L["label_fuse_explode_timer"] = "Time until explosion and refreshed when getting a kill: "
